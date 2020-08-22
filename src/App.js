2250983e57efa8269ecdc4ab3a9b43ed6c717929
Rick <rgao3736@gmail.com>
2020-08-22 14:53:42 -0500

    added read/write to db

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { db } from './firebase/config';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [ text, setText ] = useState([]);
+	useEffect(() => {
+		try {
+			db.ref().on('value', (snapshot) => {
+				let data = [];
+				snapshot.forEach((snap) => {
+					data.push(snap.val().content + ' ');
+				});
+				setText(data); // Reads from realtime DB and stores into state
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}, []);
+
+	useEffect(() => {
+		const writeData = async () => {
+			try {
+				await db.ref().push({
+					content: 'lorem2' // lorem2 should be replaced with text from Google Speech2Text API
+				});
+			} catch (e) {
+				console.log(e);
+			}
+		};
+
+		writeData();
+	}, []); // will need to re-render everytime we get new text from Google Speech2Text API
+
+	return <div className="App">{text}</div>; // print out all lines of text
 }
 
 export default App;
