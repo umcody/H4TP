997500fc7e653872c028b4be6b387d8b3a9d2625
Cody Um <yunsuum@gmail.com>
2020-08-22 18:23:51 -0500

    updates

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { db } from './firebase/config';
-function App() {
-	const [ text, setText ] = useState([]);
+import React, { useEffect, useState, useRef } from 'react';
+import { db } from '../firebase/config';
+function Test() {
+	//const messagesEndRef = useRef(null);
+	const [text, setText] = useState([]);
 	useEffect(() => {
 		try {
 			db.ref().on('value', (snapshot) => {
@@ -9,27 +10,17 @@ function App() {
 				snapshot.forEach((snap) => {
 					data.push(snap.val().content + ' ');
 				});
+				console.log(data);
 				setText(data); // Reads from realtime DB and stores into state
 			});
 		} catch (e) {
 			console.log(e);
 		}
+		//messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
 	}, []);
 
-	useEffect(() => {
-		const writeData = async () => {
-			try {
-				await db.ref().push({
-					content: 'lorem2' // lorem2 should be replaced with text from Google Speech2Text API
-				});
-			} catch (e) {
-				console.log(e);
-			}
-		};
-
-		writeData();
-	}, []); // will need to re-render everytime we get new text from Google Speech2Text API
-
-	return <div className="App">{text}</div>; // print out all lines of text
+	return <div className="transcriptCon">{text}</div> 
+		{/*<div ref={messagesEndRef} />*/}  // print out all lines of text
+	
 }
-export default App;
+export default Test;
